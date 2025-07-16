c43c928fba15df8624247cc6bc3edf70957a0d5e
Adarsh Singh <singhadarsh9540@gmail.com>
2025-07-17 04:36:04 +0530

    add

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: ["http://localhost:3000","http://job-application-tracker-hd7g1232h.vercel.app/"],
   methods: ["POST", "GET","PUT","DELETE"],
   credentials: true
 }));
