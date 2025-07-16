7f0e0f3bdde1fa825e79acc15aaf1e026c00f732
Adarsh Singh <singhadarsh9540@gmail.com>
2025-07-16 20:50:11 +0530

    fix grid

--- src/components/applicationStatus/ApplicationStatus.tsx
+++ src/components/applicationStatus/ApplicationStatus.tsx
@@ -368,7 +368,7 @@ export default function ApplicationTracker() {
         </div>
 
         {/* Kanban Board */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
           {Object.entries(statusConfig).map(([statusKey, config]) => {
             const statusApplications = groupedApplications[statusKey] || [];
             return (
